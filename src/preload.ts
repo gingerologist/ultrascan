58aab06a1ef0e1bac5a201d06ea786b9014d210a
matianfu <matianfu@gingerologist.com>
2025-08-20 02:36:59 +0800

    mdns works

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,33 @@
 // Minimal preload script - just log that it's loaded
 // console.log('Preload script loaded - Web Serial API should be available');
+
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+interface ElectronAPI {
+  openModal: () => Promise<void>;
+  submitModalData: (data: { content: string }) => Promise<{ success: boolean }>;
+  onModalDataReceived: (callback: (data: { content: string; timestamp: string }) => void) => () => void;
+  onGlobalMessage: (callback: (message: string) => void) => () => void;
+}
+
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openModal: () => ipcRenderer.invoke('open-modal'),
+  submitModalData: (data) => ipcRenderer.invoke('submit-modal-data', data),
+  onModalDataReceived: (callback) => {
+    const listener = (_event: IpcRendererEvent, data: { content: string; timestamp: string }) => {
+      callback(data);
+    };
+    ipcRenderer.on('modal-data-received', listener);
+    return () => ipcRenderer.removeListener('modal-data-received', listener);
+  },
+
+
+  onGlobalMessage: (callback) => {
+    const listener = (_event: IpcRendererEvent, message: string) => {
+      callback(message);
+    };
+    ipcRenderer.on('global-message', listener);
+    return () => ipcRenderer.removeListener('global-message', listener);
+  }
+} as ElectronAPI);
